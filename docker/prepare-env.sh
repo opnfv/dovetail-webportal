837f62407c00f0a20d0c873dec31cd98ae246dc9
pkaralis <pkaralis@intracom-telecom.com>
2018-12-06 00:43:12 +0200

    [WIP]Enable Web Portal for ONAP results
    The web portal needs to be able to read test results of the ONAP
    compliance program and display them.
    In order for the above goal to be achieved, the following two parts
    should be impacted:
    1- A new front-end should be prepared in order to handle and display
       the results
    2- The REST API should be extended in order to support the aforementioned
       operation.
    
    JIRA: DOVETAIL-669
    
    Change-Id: I36bbb6e602a67020d7e27aedbfc776f5cf4f3dc3
    Signed-off-by: pkaralis <pkaralis@intracom-telecom.com>
    Co-Authored-By: Stamatis Katsaounis <mokats@intracom-telecom.com>

--- docker/prepare-env.sh
+++ docker/prepare-env.sh
@@ -11,5 +11,4 @@ if [ "$base_url" != "" ]; then
     sudo crudini --set --existing $FILE swagger base_url $base_url
     sudo crudini --set --existing $FILE ui url $base_url
     sudo crudini --set --existing $FILE jira OAUTH_CALLBACK_URL $base_url/api/v1/auth/signin_return_jira
-    sudo crudini --set --existing $FILE lfid return_url $base_url/api/v1/auth/signin_return_cas
 fi
