f3e1bdca02d35cc3914075e7f20222c3d1b1fdf3
pkaralis <pkaralis@intracom-telecom.com>
2018-12-06 00:43:12 +0200

    [WIP]Enable Web Portal for ONAP results
    The web portal needs to be able to read test results of the ONAP
    compliance program and display them.
    In order for the above goal to be achieved, the following two parts
    should be impacted:
    1- A new front-end should be prepared in order to handle and display
       the results
    2- The REST API should be extended in order to support the aforementioned
       operation.
    
    JIRA: DOVETAIL-669
    
    Change-Id: I36bbb6e602a67020d7e27aedbfc776f5cf4f3dc3
    Signed-off-by: pkaralis <pkaralis@intracom-telecom.com>
    Co-Authored-By: Stamatis Katsaounis <mokats@intracom-telecom.com>

---
+++ opnfv_testapi/resources/review_handlers.py
@@ -0,0 +1,120 @@
+##############################################################################
+# Copyright (c) 2019 Intracom Telecom
+# mokats@intracom-telecom.com
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+from datetime import datetime
+import logging
+
+from bson import objectid
+from tornado import web, gen
+
+from opnfv_testapi.common.config import CONF
+from opnfv_testapi.common import message, raises
+from opnfv_testapi.db import api as dbapi
+from opnfv_testapi.resources import handlers, review_models
+from opnfv_testapi.tornado_swagger import swagger
+from opnfv_testapi.ui.auth import constants as auth_const
+
+
+class GenericReviewHandler(handlers.GenericApiHandler):
+    def __init__(self, review, request, **kwargs):
+        super(GenericReviewHandler, self).__init__(review, request, **kwargs)
+        self.table = 'reviews'
+        self.table_cls = review_models.Review
+
+
+class ReviewsCLHandler(GenericReviewHandler):
+    @swagger.operation(nickname="queryReviews")
+    @web.asynchronous
+    @gen.coroutine
+    def get(self):
+        def descend_limit():
+            descend = self.get_query_argument('descend', 'true')
+            return -1 if descend.lower() == 'true' else 1
+
+        def last_limit():
+            return self.get_int('last', self.get_query_argument('last', 0))
+
+        def page_limit():
+            return self.get_int('page', self.get_query_argument('page', 0))
+
+        limitations = {
+            'sort': {'_id': descend_limit()},
+            'last': last_limit(),
+            'page': page_limit(),
+            'per_page': CONF.api_results_per_page
+        }
+
+        query = yield self.set_query()
+        yield self._list(query=query, **limitations)
+        logging.debug('list end')
+
+    @swagger.operation(nickname="createReview")
+    @web.asynchronous
+    def post(self):
+        openid = self.get_secure_cookie(auth_const.OPENID)
+        if openid:
+            self.json_args['reviewer_openid'] = openid
+
+        if self.json_args['outcome'] is None:
+            self._del()
+        else:
+            self._post()
+
+    @gen.coroutine
+    def _post(self):
+        query = {'openid': self.json_args['reviewer_openid']}
+        user = yield dbapi.db_find_one('users', query)
+        if not user:
+            raises.Forbidden(message.unauthorized())
+        role = self.get_secure_cookie(auth_const.ROLE)
+        if 'reviewer' not in role.split(','):
+            raises.Unauthorized(message.no_auth())
+        test = yield dbapi.db_find_one(
+            'tests', {'id': self.json_args['test_id']})
+        if test['owner'] == self.json_args['reviewer_openid']:
+            self.finish_request({'code': 403,
+                                 'msg': 'No permision to review own results'})
+            return
+        query = {
+            'reviewer_openid': self.json_args['reviewer_openid'],
+            'test_id': self.json_args['test_id']
+        }
+        review = yield dbapi.db_find_one(self.table, query)
+        if review:
+            if review['outcome'] != self.json_args['outcome']:
+                yield dbapi.db_update(self.table, query,
+                                      {'$set': {
+                                          'outcome': self.json_args['outcome'],
+                                          'creation_date': datetime.now()}})
+            self.finish_request()
+        else:
+            self.json_args['reviewer_name'] = user['fullname']
+            self.json_args['reviewer_email'] = user['email']
+            self._create(miss_fields=[], carriers=[])
+
+    @gen.coroutine
+    def _del(self):
+        query = {'openid': self.json_args['reviewer_openid']}
+        user = yield dbapi.db_find_one('users', query)
+        if not user:
+            raises.Forbidden(message.unauthorized())
+        role = self.get_secure_cookie(auth_const.ROLE)
+        if 'reviewer' not in role.split(','):
+            raises.Unauthorized(message.no_auth())
+        test = yield dbapi.db_find_one(
+            'tests', {'id': self.json_args['test_id']})
+        if test['owner'] == self.json_args['reviewer_openid']:
+            self.finish_request({'code': 403,
+                                 'msg': 'No permision to review own results'})
+            return
+        query = {
+            'reviewer_openid': self.json_args['reviewer_openid'],
+            'test_id': self.json_args['test_id']
+        }
+        yield dbapi.db_delete(self.table, query)
+        self.finish_request()
