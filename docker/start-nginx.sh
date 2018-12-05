92da25fd50b4b4d2b91b5a102f27fd46e69ee95e
Stamatis Katsaounis <mokats@intracom-telecom.com>
2018-12-05 12:40:42 +0200

    Prepare Web Portal for ONAP integration
    This patch applies all the required changes in order to support a
    second web portal container with the onap web portal content.
    Furthermore, it creates a static lfn page in order to wrap the two
    web portals. The lfn webpage run inside a third container.
    
    Change-Id: I2f59776313bf2067dbdfb04445620b5241716ebb
    Signed-off-by: Stamatis Katsaounis <mokats@intracom-telecom.com>

--- docker/start-nginx.sh
+++ docker/start-nginx.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-FILE=/etc/nginx/sites-enabled/default
+NGINX_CONF=/etc/nginx/sites-enabled/default
+LFN_HOME=/www/static/lfn-ui/index.html
 
 if [ "$testapi_url" != "" ]; then
-    sed -i "s/server localhost:8010/server $testapi_url/" $FILE
+    sed -i "s/server cvpapi:8010/server $testapi_url/" $NGINX_CONF
+fi
+
+if [ -f "$LFN_HOME" ] && [ "$local_deployment" == 'True' ]; then
+    sed -i "s|https://verified.opnfv.org|http://ovp.localhost|" $LFN_HOME
+    sed -i "s|https://verified.onap.org|http://onap.localhost|" $LFN_HOME
 fi
 
 service supervisor start
